3253c3ce158cfcfb03938f3f8c91097425d778bd
Aman <amancse96@gmail.com>
2022-02-22 07:26:53 +0530

    last commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import { initializeApp } from '@firebase/app';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { initializeApp } from "@firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +16,7 @@ const firebaseConfig = {
   projectId: "acart-2df86",
   storageBucket: "acart-2df86.appspot.com",
   messagingSenderId: "313818473378",
-  appId: "1:313818473378:web:da48eca1399398e26508fb"
+  appId: "1:313818473378:web:da48eca1399398e26508fb",
 };
 
 // Initialize Firebase
@@ -27,7 +27,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
